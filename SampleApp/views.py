4090089d8528443608cd1af8c5f3a537adef4f79
kanyarat-tl <kanyarat.tailin@gmail.com>
2023-01-16 11:41:42 +0700

    secondpage

--- SampleApp/views.py
+++ SampleApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+def secondpage(request):
+    name = "Pen"
+    price = 10.50
+    description = "ปากกาลูกลื่นสีน้ำเงิน หัว 0.5 มิลลิเมตร"
+    quantity = 300
+    return render(request, 'secondpage.html',
+                  {'name': name, 'price': price, 'description': description, 'quantity': quantity})
 # Create your views here.
